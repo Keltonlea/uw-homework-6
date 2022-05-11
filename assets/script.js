561097144935cb5cc9215a799aab0750040978a6
Kelly Leach <101753828+Keltonlea@users.noreply.github.com>
2022-05-10 23:53:09 -0700

    background

--- assets/script.js
+++ assets/script.js
@@ -127,6 +127,68 @@ var displayUvIndex = function(index) {
     weatherContainer.appendChild(uvIndexEl);
 }
 
+var get5Day = function(city){
+    var apiKey = "7d715b654cef2b609d1bec94b45d2c07"
+    var apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`
 
+    fetch(apiURL)
+    .then(function(response){
+        response.json().then(function(data){
+           display5Day(data);
+        });
+    });
+};
+
+var display5Day = function(weather){
+    forecastContainer.textContent = ""
+    forecastTitle.textContent = "5-Day Forecast:";
+
+    var forecast = weather.list;
+        for(var i=5; i < forecast.length; i=i+8){
+       var dailyForecast = forecast[i];
+        
+       
+       var forecastEl=document.createElement("div");
+       forecastEl.classList = "card bg-primary text-light m-2";
+
+       console.log(dailyForecast)
+
+       //create date element
+       var forecastDate = document.createElement("h5")
+       forecastDate.textContent= moment.unix(dailyForecast.dt).format("MMM D, YYYY");
+       forecastDate.classList = "card-header text-center"
+       forecastEl.appendChild(forecastDate);
+
+       
+       //create an image element
+       var weatherIcon = document.createElement("img")
+       weatherIcon.classList = "card-body text-center";
+       weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${dailyForecast.weather[0].icon}@2x.png`);  
+
+       //append to forecast card
+       forecastEl.appendChild(weatherIcon);
+       
+       //create temperature span
+       var forecastTempEl=document.createElement("span");
+       forecastTempEl.classList = "card-body text-center";
+       forecastTempEl.textContent = dailyForecast.main.temp + " °F";
+
+        //append to forecast card
+        forecastEl.appendChild(forecastTempEl);
+
+       var forecastHum=document.createElement("span");
+       forecastHum.classList = "card-body text-center";
+       forecastHum.textContent = dailyForecast.main.humidity + "  %";
+
+       //append to forecast card
+       forecastEl.appendChild(forecastHum);
+
+        // console.log(forecastEl);
+       //append to five day container
+        forecastContainer.appendChild(forecastEl);
+    }
+
+}
 
 searchCityButton.addEventListener("submit", enterCity);
+
